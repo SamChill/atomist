2813d6cf9c51f79015ac76dff37f406a35ec0736
Sam Chill <samchill@gmail.com>
2012-04-26 21:43:56 -0500

    Fix to neighbor_list update() and include in init.jl

--- src/neighbor_list.jl
+++ src/neighbor_list.jl
@@ -4,20 +4,29 @@ type NeighborList
     update::Function
     distances::Array
     neighbors::Array
+    coordination::Array{Int}
 end
 
 function neighbor_list(cutoff::Real)
     neighbors = Array[]
     distances = Array[]
+    coordination = Int[]
     atoms_old = Atoms()
+
+    #This is a brute force method O(N^2). It could be improved with 
+    #the use of verlet and cell lists.
     function update(atoms::Atoms)
         if atoms_old == atoms
             return
+        else
+            atoms_old = copy(atoms)
         end
+
         N = length(atoms)
         for i = 1:N
             push(neighbors, Array[])
             push(distances, Float64[])
+            push(coordination, 0)
             for j = 1:N
                 if i == j
                     continue
@@ -26,9 +35,10 @@ function neighbor_list(cutoff::Real)
                 if r < cutoff
                     push(neighbors[i], [i,j])
                     push(distances[i], r)
+                    coordination[i] += 1
                 end
             end
         end
     end
-    NeighborList(update, distances, neighbors)
+    NeighborList(update, distances, neighbors, coordination)
 end
